06260d66e50441cbab1286ffa76c75bb528f3379
SaolGhra <DeathCRystalYT@Gmail.com>
2024-08-06 00:15:20 +0100

    V1.2 1 - Boxes are placed in the correct location 2 - Durability z-indexing is correct 3 - Textures on the boxes are aligned and correct 4 - Code is efficient
    Downside:
    1 - Durability bar is incorrect, will fix on next push

--- src/client/java/com/saolghra/armor_hud/client/ArmorHudOverlay.java
+++ src/client/java/com/saolghra/armor_hud/client/ArmorHudOverlay.java
@@ -7,7 +7,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
 
 public class ArmorHudOverlay {
-    private static final Identifier HOTBAR_TEXTURE = Identifier.of("minecraft", "textures/gui/widgets.png");
+    private static final Identifier HOTBAR_TEXTURE = Identifier.of("armor_hud", "textures/gui/hotbar_texture.png");
 
     public void renderArmorUI(DrawContext context, RenderTickCounter tickDelta) {
         MinecraftClient client = MinecraftClient.getInstance();
@@ -18,16 +18,16 @@ public class ArmorHudOverlay {
         ItemStack[] armorItems = client.player.getInventory().armor.toArray(new ItemStack[0]);
 
         // Position for the armor boxes
-        int boxSize = 20;
-        int spacing = 4;
+        int boxSize = 22;
+        int spacing = 2; // Was 4
 
         // Get screen width and height
         int screenWidth = client.getWindow().getScaledWidth();
         int screenHeight = client.getWindow().getScaledHeight();
 
         // Calculate the position for the offhand slot
-        int offhandSlotX = screenWidth / 2 + 10;
-        int offhandSlotY = screenHeight - 60;
+        int offhandSlotX = screenWidth / 2 - 124; // Was 2 - 120
+        int offhandSlotY = screenHeight - 22; // 22 (Copy boxSize)
 
         // Offset based on offhand slot position
         int xOffset = offhandSlotX - (armorItems.length * (boxSize + spacing) + spacing);
@@ -43,24 +43,79 @@ public class ArmorHudOverlay {
             ItemStack armorItem = armorItems[i];
 
             if (!armorItem.isEmpty()) {
-                // Draw box background
+                // Reused variable redeclaration.
                 int armorSpacing = (armorItems.length - 1 - i) * (boxSize + spacing);
 
-                drawTexture(context, xOffset + armorSpacing, yOffset, 0, 0, boxSize, boxSize, 256, 256);
+                // Draw box background
+                drawTexture(context, xOffset + armorSpacing, yOffset, 0, 0, boxSize, boxSize, 22, 22);
 
                 // Draw armor icon
-                context.drawItem(armorItem, xOffset + armorSpacing + 2, yOffset + 2);
+                context.getMatrices().push();
+                context.getMatrices().translate(0, 0, 0);
+                context.drawItem(armorItem, xOffset + armorSpacing + (boxSize - 16) / 2, yOffset + (boxSize - 16) / 2);
+                context.getMatrices().pop();
 
                 // Draw the durability bar
-                int durability = armorItem.getMaxDamage() - armorItem.getDamage();
-                int durabilityWidth = (int) ((durability / (float) armorItem.getMaxDamage()) * boxSize);
-                fill(context, xOffset + armorSpacing, yOffset + boxSize - 4, xOffset + armorSpacing + durabilityWidth, yOffset + boxSize, 0xFF00FF00); // Green for durability
+            }
+        }
+
+        // Draw durability bar separately so it is on top
+        for (int i = armorItems.length - 1; i >= 0; i--) {
+            ItemStack armorItem = armorItems[i];
+
+            if (!armorItem.isEmpty()) {
+                int armorSpacing = (armorItems.length - 1 - i) * (boxSize + spacing);
+
+                // Draw the durability
+                context.getMatrices().push();
+                context.getMatrices().translate(0, 0, 200);
+                drawDurabilityBar(context, xOffset + armorSpacing, yOffset + boxSize - 6, boxSize, armorItem);
+                context.getMatrices().pop();
             }
         }
     }
 
     private void drawTexture(DrawContext context, int x, int y, int u, int v, int width, int height, int textureWidth, int textureHeight) {
         context.drawTexture(HOTBAR_TEXTURE, x, y, u, v, width, height, textureWidth, textureHeight);
+    }
+
+    private void drawDurabilityBar(DrawContext context, int x, int y, int width, ItemStack item) {
+        int maxDamage = item.getMaxDamage();
+        int damage = item.getDamage();
+        int durability = maxDamage - damage;
+
+        // Total width of the durability bar
+        int barWidth = width - 8;
+        int barX = x + 4;
+        int durabilityWidth = (int) ((durability / (float) maxDamage) * barWidth);
+
+        // height of the durability bar
+        int barHeight = 2;
+
+        // Draw the full bar with color transitions
+        for (int i = 0; i < barWidth; i++) {
+            int segmentX = barX + i;
+            int segmentColor;
+
+            float ratio = (i / (float) barWidth);
+
+            // Determine color based on remaining durability ratio
+            if (durabilityWidth > i) {
+                if (ratio > 0.75) {
+                    segmentColor = 0xFF00FF00; // Green
+                } else if (ratio > 0.25) {
+                    segmentColor = 0xFFFFFF00; // Yellow
+                } else {
+                    segmentColor = 0xFFFF0000; // Red
+                }
+            } else {
+                // Lost durability area
+                segmentColor = 0xFF000000; // Black
+            }
+
+            // Draw each segment of the durability bar
+            fill(context, segmentX, y, segmentX + 1, y + barHeight, segmentColor);
+        }
     }
 
     private void fill(DrawContext context, int x1, int y1, int x2, int y2, int color) {
